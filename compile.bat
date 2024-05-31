9ce763d54faf9792d4580585d87532db7ae86a45
lingeringwillx <111698406+lingeringwillx@users.noreply.github.com>
2024-05-31 13:51:15 +0300

    Faster compression

--- compile.bat
+++ compile.bat
@@ -1,7 +1,7 @@
-g++ -c dbpf.cpp
+g++ -c -O2 dbpf.cpp
 g++ -shared dbpf.o -o dbpf32.dll
 
-x86_64-w64-mingw32-g++ -c dbpf.cpp
+x86_64-w64-mingw32-g++ -c -O2 dbpf.cpp
 x86_64-w64-mingw32-g++ -shared dbpf.o -o dbpf64.dll
 
 del dbpf.o
